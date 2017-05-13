72710d80f777dd24292427c3968ac32ab3c33d40
Alex Autem <autem.alex@gmail.com>
2017-05-13 15:00:03 -0400

    Update YouTube API key in App component

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -6,7 +6,7 @@ import VideoColumn from './VideoColumn.jsx';
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || window.YOUTUBE_KEY;
 
-console.log('YouTube Key:', YOUTUBE_API_KEY);
+console.log('YouTube Key:', process.env.YOUTUBE_API_KEY);
 
 export default class App extends Component {
   constructor(props) {
