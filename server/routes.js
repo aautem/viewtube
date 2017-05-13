45b81fffcad324349e02a36ef7fd3d36ff49ac6a
Alex Autem <autem.alex@gmail.com>
2017-05-13 17:17:03 -0400

    Update API call

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app, express) {
   app.get('/api/keys/youtube', function(req, res) {
     if (process.env.YOUTUBE_API_KEY) {
-      res.send(JSON.stringify(process.env.YOUTUBE_API_KEY);
+      res.send(JSON.stringify(process.env.YOUTUBE_API_KEY));
     } else {
       res.send(JSON.stringify(null));
     }
