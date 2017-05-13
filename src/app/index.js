ee4034ac95de3f0017475a02e02bf62c6dc00b22
Alex Autem <autem.alex@gmail.com>
2017-05-13 17:01:28 -0400

    Update where ReactDOM is called

--- src/app/index.js
+++ src/app/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-let YOUTUBE_API_KEY;
-
 (function() {
   return fetch('https://view-tube.herokuapp.com/api/keys/youtube').then(function(response) {
     if (response.ok) {
       return response.json();
     }
   }).then(function(key) {
-    YOUTUBE_API_KEY = key;
+    ReactDOM.render(<App YOUTUBE_API_KEY={key} />, document.getElementById('app'));
   });
 })();
-
-ReactDOM.render(<App YOUTUBE_API_KEY={YOUTUBE_API_KEY} />, document.getElementById('app'));
