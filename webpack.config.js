92696a98064b0d9a018b7b65a1983e7156a6a838
Alex Autem <autem.alex@gmail.com>
2017-05-13 01:47:25 -0400

    Update config files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+var config = {
+  entry: [APP_DIR + '/index.js', APP_DIR + '/stylesheets/main.scss'],
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1'
+        })
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '/main.bundle.css',
+      allChunks: true
+    })
+  ]
+};
+
+module.exports = config;
