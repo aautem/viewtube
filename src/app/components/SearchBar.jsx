5f2c8228535e233fc37634eb9e6328c3d1df5f7a
Alex Autem <autem.alex@gmail.com>
2017-05-13 18:56:31 -0400

    Add search button to search bar

--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -4,6 +4,7 @@ const SearchBar = (props) => {
   return (
     <div className="search col-8">
       <input type="text" placeholder="Search ViewTube..." />
+      <button>Search</button>
     </div>
   );
 };
