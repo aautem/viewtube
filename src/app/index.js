821f4d649443dd61335119b89f3c7832e2a8087b
Alex Autem <autem.alex@gmail.com>
2017-05-15 12:08:18 -0400

    Update App

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-const endpoint = 'https://view-tube.herokuapp.com/api/keys/youtube';
+// const endpoint = 'https://view-tube.herokuapp.com/api/keys/youtube';
+const endpoint = 'http://localhost:3000/api/keys/youtube';
 
 (function() {
   return fetch(endpoint).then(function(response) {
