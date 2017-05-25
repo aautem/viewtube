e2f79dee29fcd6388b8525eaeac0c178a36061cd
Alex Autem <autem.alex@gmail.com>
2017-05-25 09:43:25 -0700

    Add deployment endpoint

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-// const endpoint = 'https://view-tube.herokuapp.com/api/keys/youtube';
-const endpoint = 'http://localhost:3000/api/keys/youtube';
+const endpoint = 'https://view-tube.herokuapp.com/api/keys/youtube';
+// const endpoint = 'http://localhost:3000/api/keys/youtube';
 
 (function() {
   return fetch(endpoint).then(function(response) {
