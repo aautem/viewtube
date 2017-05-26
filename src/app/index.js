5d77448108e0bb0517e8fb6120f8f1298d86dfc8
Alex Autem <autem.alex@gmail.com>
2017-05-25 17:39:10 -0700

    Refactor api calls to use Axios

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './components/App.jsx';
 
 // const endpoint = 'https://view-tube.herokuapp.com/api/keys/youtube';
 const endpoint = 'http://localhost:3000/api/keys/youtube';
 
 (function() {
-  return fetch(endpoint).then(function(response) {
-    if (response.ok) {
-      return response.json();
-    }
-  }).then(function(key) {
+  axios.get(endpoint).then(function(response) {
+    var key = response.data;
     if (!key) {
       key = window.YOUTUBE_KEY;
     }
+    console.log('KEY:', key);
     ReactDOM.render(<App YOUTUBE_API_KEY={key} />, document.getElementById('app'));
   });
 })();
